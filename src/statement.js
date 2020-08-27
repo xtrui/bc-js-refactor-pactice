82032ea4985e86842b93a96519ce8f2b1f037b56
Ray <ray.wen@oocl.com>
2020-08-27 17:21:25 +0800

    [Ray] refactor: delete useless code.

--- src/statement.js
+++ src/statement.js
@@ -5,20 +5,6 @@ function calculateCredits(volumeCredits, perf, play) {
     return volumeCredits;
 }
 
-function createHtmlResult(result, format, totalAmount, volumeCredits) {
-    result = '<h1>Statement for BigCo</h1>\n' +
-        '<table>\n' +
-        '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
-        ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
-        ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
-        ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
-        '</table>\n' +
-        '<p>Amount owed is <em>$1,730.00</em></p>\n' +
-        '<p>You earned <em>47</em> credits</p>\n'
-
-    return "";
-}
-
 function getTotalAmount(plays, invoice) {
     let totalAmount = 0;
 
@@ -67,17 +53,33 @@ function getTextResult(invoice, statementData, totalAmount, volumeCredits) {
     return result;
 }
 
+function getHtmlResult(invoice, statementData, totalAmount, volumeCredits) {
+    return undefined;
+}
+
 function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let statementData = [];
+    let totalAmount;
+    let volumeCredits;
+    let statementData;
 
     totalAmount = getTotalAmount(plays, invoice);
     volumeCredits = getVolumeCredit(plays, invoice);
     statementData = getStatementData(plays, invoice)
 
-    let result = getTextResult(invoice, statementData, totalAmount, volumeCredits);
-    return result;
+    return getTextResult(invoice, statementData, totalAmount, volumeCredits);
+}
+
+
+function htmlStatement(invoice, plays) {
+    let totalAmount;
+    let volumeCredits;
+    let statementData;
+
+    totalAmount = getTotalAmount(plays, invoice);
+    volumeCredits = getVolumeCredit(plays, invoice);
+    statementData = getStatementData(plays, invoice)
+
+    return getHtmlResult(invoice, statementData, totalAmount, volumeCredits);
 }
 
 function calculateAmount(thisAmount, perf, play) {
