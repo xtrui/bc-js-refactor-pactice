f0c203cf38911e9f5ef6dc646482546e0a6302c6
Ray <ray.wen@oocl.com>
2020-08-27 15:43:42 +0800

    [Ray] refactor: refactor statement.js

--- src/statement.js
+++ src/statement.js
@@ -1,45 +1,78 @@
-function statement (invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
-    // add volume credits
+function calculateCredits(volumeCredits, perf, play) {
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
     if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    //print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
-  }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits \n`;
-  return result;
+    return volumeCredits;
+}
+
+function createHtmlResult(result, format, totalAmount, volumeCredits) {
+    result = '<h1>Statement for BigCo</h1>\n' +
+        '<table>\n' +
+        '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
+        ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
+        ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
+        ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
+        '</table>\n' +
+        '<p>Amount owed is <em>$1,730.00</em></p>\n' +
+        '<p>You earned <em>47</em> credits</p>\n'
+
+    return "";
+}
+
+function statement(invoice, plays) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    const format = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+    }).format;
+
+    for (let perf of invoice.performances) {
+        const play = plays[perf.playID];
+        let thisAmount = 0;
+        thisAmount = calculateAmount(thisAmount, perf, play)
+        // add volume credits
+        volumeCredits = calculateCredits(volumeCredits, perf, play);
+        //print line for this order
+        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+        totalAmount += thisAmount;
+    }
+    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `You earned ${volumeCredits} credits \n`;
+    return result;
+}
+
+function calculateAmount(thisAmount, perf, play) {
+    if (play.type === 'tragedy') {
+        thisAmount = calculateTragedyAmount(thisAmount, perf);
+    } else if (play.type === 'comedy') {
+        thisAmount = calculateComedyAmount(thisAmount, perf);
+    } else {
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    return thisAmount;
+}
+
+
+function calculateTragedyAmount(thisAmount, perf) {
+    thisAmount = 40000;
+    if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+    }
+    return thisAmount;
+}
+
+function calculateComedyAmount(thisAmount, perf) {
+    thisAmount = 30000;
+    if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+    }
+    thisAmount += 300 * perf.audience;
+    return thisAmount;
 }
 
 module.exports = {
-  statement,
+    statement,
 };
